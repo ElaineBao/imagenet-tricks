c5eb40bdc33ac829aab41f930e8d75d6e0ecaba0
elainebao <elaine.bao@foxmail.com>
2019-02-22 21:56:05 +0800

    update

--- model_zoo/__init__.py
+++ model_zoo/__init__.py
@@ -15,6 +15,17 @@ from .deform_resnetd import deform_resnetd50
 from .deform_resnetd import deform_resnetd101
 from .deform_resnetd import deform_resnetd152
 
+from .senet import senet154
+from .senet import se_resnetd50
+from .senet import se_resnetd101
+from .senet import se_resnetd152
+from .senet import se_resnextd50_32x4d
+from .senet import se_resnextd50_64x4d
+from .senet import se_resnextd101_32x4d
+from .senet import se_resnextd101_64x4d
+from .senet import se_resnextd152_32x4d
+from .senet import se_resnextd152_64x4d
+
 # torchvision models
 from torchvision.models import resnet50
 from torchvision.models import resnet101
